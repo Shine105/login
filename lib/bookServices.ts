0aca5218091143a739bcd7fc3caa451b3589445b
Simran <btech10760.21@bitmesra.ac.in>
2025-04-18 15:22:49 +0530

    creating lib

---
+++ lib/bookServices.ts
@@ -0,0 +1,18 @@
+// lib/bookService.ts
+
+export async function fetchBookFromGoogle(volumeId: string) {
+    const res = await fetch(`https://www.googleapis.com/books/v1/volumes/${volumeId}`);
+    if (!res.ok) return null;
+  
+    const data = await res.json();
+    return {
+      id: data.id,
+      title: data.volumeInfo.title,
+      authors: data.volumeInfo.authors ?? [],
+      description: data.volumeInfo.description ?? '',
+      coverImage: data.volumeInfo.imageLinks?.thumbnail ?? '',
+      publisher: data.volumeInfo.publisher ?? '',
+      publishedDate: data.volumeInfo.publishedDate ?? '',
+    };
+  }
+  
